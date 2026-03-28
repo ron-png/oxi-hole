b8dfa89949166cdd76b399e0f1e95c36b6c75963
ron-png <41197190+ron-png@users.noreply.github.com>
2026-03-28 21:06:44 +0000

    feat: inject OXI_HOLE_VERSION as rustc environment variable in build script

--- build.rs
+++ build.rs
@@ -1,3 +1,8 @@
 fn main() {
     println!("cargo::rerun-if-env-changed=OXI_HOLE_VERSION");
+    // If OXI_HOLE_VERSION is set (CI builds), inject it as a rustc cfg env.
+    // This guarantees cargo tracks the value and recompiles when it changes.
+    if let Ok(version) = std::env::var("OXI_HOLE_VERSION") {
+        println!("cargo::rustc-env=OXI_HOLE_VERSION={}", version);
+    }
 }
